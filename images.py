2178a3e593796f83009774985552a676e950108d
LuisGomez24 <luisfergs24@gmail.com>
2022-08-20 05:45:11 -0600

    Documentation added. Ascii art deleted. Image viewer added.

--- images.py
+++ images.py
@@ -1,20 +1,24 @@
-import pywhatkit as kt
+#! /usr/bin/env python
+
+"""_summary_"""
+import cv2
 
 SRC_DIR  = 'card_img/'
-DEST_DIR = 'card_ascii/' 
 
 def save_img(card_name, response_img):
-    
+    """_summary_
+
+    Args:
+        card_name (string):Card's name searched
+        response_img (json): Data obtained by API
+    """
+
     card_path = f'{SRC_DIR}{card_name}.jpg'
-    
+
     with open(card_path, 'wb') as file:
         for chunk in response_img.iter_content(1024):
             file.write(chunk)
-            
-def save_ascii_img(card_name):
-    
-    src_path  = f'{SRC_DIR}{card_name}.jpg'
-    dest_path = f'{DEST_DIR}{card_name}'
-    
-    kt.image_to_ascii_art(src_path, dest_path)
 
+    image = cv2.imread(card_path, 1)
+    cv2.imshow(card_name, image)
+    cv2.waitKey(0)
