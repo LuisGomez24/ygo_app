b52cebf0b004a0e0e07209a8b997404f53327293
LuisGomez24 <luisfergs24@gmail.com>
2022-08-20 05:44:10 -0600

    Documentation added

--- api.py
+++ api.py
@@ -1,14 +1,19 @@
+"""Libraries"""
 import requests
-import json
 
 URL = 'https://db.ygoprodeck.com/api/v7/cardinfo.php'
 
+
 def confirm_request(request):
-    
+    """_summary_
+
+    Args:
+        request (function): function to decorate
+    """
     def wrap(*args, **kwargs):
-        
+
         response = request(*args, **kwargs)
-        
+
         match response.status_code:
             case 200:
                 print('Card successfully found...')
@@ -20,9 +25,18 @@ def confirm_request(request):
             case _:
                 print('Unidentified error...')
         return (False, response)
-    
+
     return wrap
-    
+
 @confirm_request
 def get_request(url):
+    """_summary_
+
+    Args:
+        url (string): URL to get data from API
+
+    Returns:
+        json: Data from API request
+    """
+
     return requests.get(url)
--- main.py
+++ main.py
@@ -1,40 +1,39 @@
+"""_summary_"""
 import json
 import api
 import images
 
-
 def main():
-    
+    """_summary_"""
     card_name = input("Digite el nombre de la carta que desea buscar: ").strip().lower()
     url_request = f'{api.URL}?name={card_name}'
-    
+
     status, response = api.get_request(url_request)
-    
+
     if status:
-        
+
         card_name = card_name.replace(' ', '_')
         card_data = f'card_data/{card_name}.txt'
         response_content = response.content.decode('utf-8')
         data_json = json.loads(response_content)
         data = data_json['data'][0]
-        
-        with open(card_data, 'w+') as file:
+
+        with open(card_data, 'w+', encoding='utf-8') as file:
             file.truncate(0)
-        
+
         for key in data:
             match key:
-                case 'card_sets' | 'card_prices': 
+                case 'card_sets' | 'card_prices':
                     pass
                 case 'card_images':
                     status, response_img = api.get_request(data[key][0]['image_url'])
-                    
+
                     if status:
                         images.save_img(card_name, response_img)
-                        images.save_ascii_img(card_name)
                 case _:
-                    
-                    with open(card_data, 'a') as file:
+
+                    with open(card_data, 'a', encoding='utf-8') as file:
                         file.write(key + ' : ' + str(data[key]) + '\n')
-                        
+
 if __name__ == '__main__':
     main()
